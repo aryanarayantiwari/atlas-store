cbccacf8c800550c1a2304dd8ac4cccd287299d8
Arya <aryanarayantiwari@gmail.com>
2022-01-13 14:51:10 +0530

    update small screen behaviour

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,10 +24,10 @@ const Header = () => {
     return (
         <header className='flex mx-2 md:mx-10 my-2 py-4 border-b items-center border-gray-300'>
             <Link href="/"><h1 className=' cursor-pointer text-base md:text-2xl font-bold hover:text-red-400'>Atlas Store</h1></Link>
-            <div className="search w-30 flex space-x-2 px-1 py-2 items-center md:ml-auto border border-gray-300 focus-within:ring-2 sm:w-min rounded-md">
+            <div className="search p-1.5 w-30 ml-5 flex md:space-x-2 md:px-1 md:py-2 items-center md:ml-auto border border-gray-300 focus-within:ring-2 rounded-md">
             <SearchIcon className=' w-6 h-6 text-gray-300' />
             <form onSubmit={handleSubmit}>
-                <input type="search" placeholder='Search' className=' focus:outline-none' onChange={(e) => setSearchTerm(e.target.value)}
+                <input type="search" placeholder='Search' className='sm:text-sm md:text-base focus:outline-none' onChange={(e) => setSearchTerm(e.target.value)}
                 value={searchTerm}
                 />
             </form>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,10 +5,10 @@ const Hero = () => {
         <div>
             <main className="mt-8 mx-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className=" text-center">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                 <span className="block xl:inline">Amazing developer wearables</span>{' '}
               </h1>
-              <p className="mt-6 text-gray-500 text-xl text-center mx-auto max-w-5xl"> 
+              <p className="mt-6 text-gray-500 text-xl text-center mx-auto max-w-lg md:max-w-5xl"> 
                 Get your favorite developer tee, hat, hoodie etc. that you have been searching for 
                 at the best price possible and show it off to your friends. 
               </p>
