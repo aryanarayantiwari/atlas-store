1bee26cc14d57c931c57d9591368e56789781d67
Arya <aryanarayantiwari@gmail.com>
2022-01-31 12:06:39 +0530

    update payment pages

--- pages/cancel.tsx
+++ pages/cancel.tsx
@@ -4,8 +4,8 @@ function cancel() {
   return (
     <div className="flex flex-col text-4xl font-semibold items-center justify-center min-h-screen  bg-green-400">
       <h2>Looks like you canceled the order.</h2>
-      <Link href="/" className=" bg-white bg-opacity-30 px-4 py-2 rounded-2xl">
-        Go to home page
+      <Link href="/" >
+        <span className="bg-white bg-opacity-30 text-2xl mt-5 px-4 py-2 rounded-2xl cursor-pointer hover:bg-opacity-95">Go to home page</span>
       </Link>
     </div>
   );
--- pages/success.tsx
+++ pages/success.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 function success() {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen  bg-green-400">
         <h2 className="text-4xl font-semibold">Thanks for shopping with us</h2>
+        <Link href="/" >
+        <span className=" bg-white bg-opacity-30 px-4 py-2 mt-5 rounded-2xl cursor-pointer hover:bg-opacity-95">Go to home page</span>
+      </Link>
       </div>
     );
   }
